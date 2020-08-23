d76fd4a3bc0d54a43f879d711a12e55c9755a0a1
k-dot-11 <karnatheveer@gmail.com>
2020-08-23 14:16:45 +0530

    Initial commit

---
+++ routes/testAPI.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.send('“API is working properly');
+});
+
+module.exports = router;
